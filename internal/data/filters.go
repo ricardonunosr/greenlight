8893e7952a38f72e3406a25ba1598ee9a3ec3f00
Ricardo Ribeiro <ricardonunosr@gmail.com>
2021-12-01 15:07:02 +0000

    Chapter 9.7

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"strings"
+
 	"greenlight.ricardonunosr.dev/internal/validator"
 )
 
@@ -18,4 +20,22 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
